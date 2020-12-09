d65cb0ac7dc539b9828ba7907a3623b5e704d739
CCBaxter84 <troyceclark@gmail.com>
2020-12-09 14:20:08 -0700

    Stubbed out server routes and tested them

--- server/index.js
+++ server/index.js
@@ -7,4 +7,26 @@ const {
 
 app.use(express.json());
 
+app.get('/reviews/:product_id/list', (req, res) => {
+  res.status(200).json({ msg: 'your mom' });
+});
+
+app.get('/reviews/:product_id/meta', (req, res) => {
+  res.status(200).json({ msg: 'your mom' });
+});
+
+app.post('/reviews/:product_id', (req, res) => {
+  res.status(200).json({ msg: 'your mom' });
+});
+
+app.put('/reviews/helpful/:review_id', (req, res) => {
+  res.status(200).json({ msg: 'your mom' });
+});
+
+app.put('/reviews/report/:review_id', (req, res) => {
+  res.status(200).json({ msg: 'your mom' });
+});
+
+
+
 app.listen(PORT, () => console.log(`Server is running and listening on port ${PORT}`));
