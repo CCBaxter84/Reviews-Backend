3d7bc3b7c586f05f0db242cea04c6f5d45c8018f
CCBaxter84 <troyceclark@gmail.com>
2020-12-12 18:59:34 -0700

    Added getPhotos query

--- db/queries.js
+++ db/queries.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 function getReviews(productId, callback) {
-  pool.query('SELECT * FROM reviews WHERE product_id = $1 AND reported IS NULL', [productId], (err, results) => {
+  pool.query('SELECT * FROM partreviews WHERE product_id = $1 AND reported IS NULL', [productId], (err, results) => {
     if (err) {
       callback(err, null);
     } else {
@@ -20,8 +20,18 @@ function getReviews(productId, callback) {
   });
 }
 
+function getPhotos(reviewId, callback) {
+  pool.query('SELECT * FROM photos WHERE review_id = $1', [reviewId], (err, results) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  })
+}
+
 function addReview(params, callback) {
-  pool.query('INSERT INTO reviews (rating, recommend, response, body, date, reviewer_name, helpfulness, reported, product_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', params, (err, result) => {
+  pool.query('INSERT INTO partreviews (rating, recommend, response, body, date, reviewer_name, helpfulness, reported, product_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', params, (err, result) => {
     if (err) {
       callback(err, null);
     } else {
@@ -31,7 +41,7 @@ function addReview(params, callback) {
 }
 
 function markAsHelpful(reviewId, callback) {
-  pool.query('UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id = $1', [reviewId], (err, result) => {
+  pool.query('UPDATE partreviews SET helpfulness = helpfulness + 1 WHERE review_id = $1', [reviewId], (err, result) => {
     if (err) {
       callback(err, null);
     } else {
@@ -41,7 +51,7 @@ function markAsHelpful(reviewId, callback) {
 }
 
 function reportReview(reviewId, callback) {
-  pool.query('UPDATE reviews SET reported = TRUE WHERE review_id = $1', [reviewId], (err, result) => {
+  pool.query('UPDATE partreviews SET reported = TRUE WHERE review_id = $1', [reviewId], (err, result) => {
     if (err) {
       callback(err, null);
     } else {
@@ -61,7 +71,7 @@ function getCharacteristics(productId, callback) {
 }
 
 function getMetadata(productId, callback) {
-  pool.query('SELECT rating, recommend FROM reviews WHERE product_id = $1 AND reported IS NULL', [productId], (err, results) => {
+  pool.query('SELECT rating, recommend FROM partreviews WHERE product_id = $1 AND reported IS NULL', [productId], (err, results) => {
     if (err) {
       callback(err, null);
     } else {
@@ -72,6 +82,7 @@ function getMetadata(productId, callback) {
 
 module.exports = {
   getReviews,
+  getPhotos,
   addReview,
   markAsHelpful,
   reportReview,
